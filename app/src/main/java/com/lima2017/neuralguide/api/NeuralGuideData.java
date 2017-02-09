bf2a4c83ab8ae09ab4e3ce5bbbebd67f534792fc
tamaranorman <tamara_norman@hotmail.com>
2017-02-09 19:44:14 +0000

    Added in Json decoding of result

---
+++ app/src/main/java/com/lima2017/neuralguide/api/NeuralGuideData.java
@@ -0,0 +1,8 @@
+package com.lima2017.neuralguide.api;
+
+/**
+ * Created by Tamara Norman on 09/02/2017.
+ */
+
+public class NeuralGuideData {
+}
---
+++ app/src/main/java/com/lima2017/neuralguide/api/NeuralGuideResult.java
@@ -0,0 +1,8 @@
+package com.lima2017.neuralguide.api;
+
+/**
+ * Created by Tamara Norman on 08/02/2017.
+ */
+
+public class NeuralGuideResult {
+}
