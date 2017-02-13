a028acec3e5ffdf9fd381b13dbcfe6b2b044eae6
henry-thompson <henryithompson@btinternet.com>
2017-02-13 22:58:23 +0000

    Remove unused imports

--- app/src/main/java/com/lima2017/neuralguide/NeuralGuideFragment.java
+++ app/src/main/java/com/lima2017/neuralguide/NeuralGuideFragment.java
@@ -4,7 +4,6 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
-import android.service.voice.AlwaysOnHotwordDetector;
 import android.speech.tts.TextToSpeech;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
