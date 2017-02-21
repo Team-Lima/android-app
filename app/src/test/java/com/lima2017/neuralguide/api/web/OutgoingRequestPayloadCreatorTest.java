8b85fe720218ef45945ddf6217c32d4dbd9b1cad
tamaranorman <tamara_norman@hotmail.com>
2017-02-21 11:51:58 +0000

    Tests on OutgoingRequestPayloadCreator

---
+++ app/src/test/java/com/lima2017/neuralguide/api/web/OutgoingRequestPayloadCreatorTest.java
@@ -0,0 +1,36 @@
+package com.lima2017.neuralguide.api.web;
+
+import com.lima2017.neuralguide.api.NeuralGuideData;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * Created by Tamara Norman on 21/02/2017.
+ */
+
+public class OutgoingRequestPayloadCreatorTest {
+    @Test
+    public void when_given_valid_byte_array_produces_string () throws Exception {
+        byte[] bytes = new byte[10];
+        for (byte i = 0; i < 10; i++){
+            bytes[i] = i;
+        }
+
+        OutgoingRequestPayloadCreator instance = new OutgoingRequestPayloadCreator();
+        String result = instance.generateOutgoingJsonString(bytes);
+
+        assertEquals("{\"data\":\"AAECAwQFBgcICQ==\"}", result);
+    }
+
+    @Test
+    public void when_given_empty_byte_array_sends_Json_with_null_data () throws Exception {
+        byte[] bytes = null;
+
+        OutgoingRequestPayloadCreator instance = new OutgoingRequestPayloadCreator();
+        String result = instance.generateOutgoingJsonString(bytes);
+
+        assertEquals("{\"data\":null}", result);
+    }
+}
