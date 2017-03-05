0b3785af6b5a254a46024b4644955a58835eb728
tamaranorman <tamara_norman@hotmail.com>
2017-03-05 16:55:40 +0000

    Added test coverage for set maker method in mapper

--- app/src/test/java/com/lima2017/neuralguide/api/StringToImprovementTipMappingTest.java
+++ app/src/test/java/com/lima2017/neuralguide/api/StringToImprovementTipMappingTest.java
@@ -4,6 +4,8 @@ import com.lima2017.neuralguide.api.web.StringToImprovementTipMapping;
 
 import org.junit.Test;
 
+import java.util.Set;
+
 import java8.util.Optional;
 
 import static junit.framework.Assert.assertFalse;
@@ -15,7 +17,7 @@ public class StringToImprovementTipMappingTest {
     private static final String DARK = "dark";
 
     @Test
-    public void maps_blurry_to_enum () {
+    public void maps_blurry_to_enum() {
         StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
         Optional<ImprovementTip> result = instance.getImprovementTip(BLURRY);
 
@@ -23,7 +25,7 @@ public class StringToImprovementTipMappingTest {
     }
 
     @Test
-    public void maps_dark_to_enum () {
+    public void maps_dark_to_enum() {
         StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
         Optional<ImprovementTip> result = instance.getImprovementTip(DARK);
 
@@ -31,9 +33,35 @@ public class StringToImprovementTipMappingTest {
     }
 
     @Test
-    public void when_given_incorrect_string_returns_empty () {
+    public void when_given_incorrect_string_returns_empty() {
         StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
         Optional<ImprovementTip> result = instance.getImprovementTip("hello");
         assertFalse(result.isPresent());
+    }
+
+    @Test
+    public void when_given_empty_array_returns_empty_hash_set() {
+        StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
+        Set<ImprovementTip> result = instance.createImprovementTipsSet(null);
+
+        assertEquals(true, result.isEmpty());
+    }
+
+    @Test
+    public void when_given_array_with_correct_strings_returns_set() {
+        StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
+        Set<ImprovementTip> result = instance.createImprovementTipsSet(new String[]{"dark", "blurry"});
+
+        assertEquals(true, result.contains(ImprovementTip.TooDark));
+        assertEquals(true, result.contains(ImprovementTip.TooBlurry));
+    }
+
+    @Test
+    public void when_given_array_contains_a_mix_of_valid_and_invalid_set_contains_only_valid() {
+        StringToImprovementTipMapping instance = new StringToImprovementTipMapping();
+        Set<ImprovementTip> result = instance.createImprovementTipsSet(new String[]{"dark", "hello"});
+
+        assertEquals(true, result.contains(ImprovementTip.TooDark));
+        assertEquals(1, result.size());
     }
 }
